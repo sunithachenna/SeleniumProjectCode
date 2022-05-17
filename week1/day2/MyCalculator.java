339fd02da8ce8f329c9c00aa326d8033f50fb1ff
sunithadinesh <105366555+sunithadinesh@users.noreply.github.com>
2022-05-17 11:42:36 +0530

    Add files via upload

---
+++ week1/day2/MyCalculator.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class MyCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Calculator myCalculator = new Calculator();
+System.out.println("Addition result: "+myCalculator.add(9,8));
+System.out.println("Subtraction result: "+myCalculator.sub(23.5, 23.5));
+System.out.println("Multiplication result: "+myCalculator.mul(56.78, 21.56));
+System.out.println("Division result : "+myCalculator.div(90, 10));
+
+	}
+
+}
